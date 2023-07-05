7e661e941557b832e169835698271e6b930d1ba8
Galbeyte <s.galbeyte@gmail.com>
2023-07-05 14:33:56 -0400

    Creating DB package.

---
+++ internal/db/db.go
@@ -0,0 +1,41 @@
+package db
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Store struct {
+	db * sqlx.DB
+}
+
+
+// New - returns a new store or error
+func New() (Store, error) {
+	dbUsername := os.Getenv("DB_USERNAME")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbTable := os.Getenv("DB_PORT")
+	dbPort := os.Getenv("DB_PORT")
+	dbSSLMode := os.Getenv("DB_SSL_MODE")
+
+	connectionString := fmt.Sprintf(
+		"host=%s port=%s user%s dbname=%s password=%s sslmode=%s",
+		dbHost,
+		dbPort,
+		dbUsername,
+		dbTable,
+		dbPassword,
+		dbSSLMode,
+	)
+
+	db, err := sqlx.Connect("postgres", connectionString)
+	if err != nil {
+		return Store{}, err
+	}
+	return Store{
+		db: db,
+	}, nil
+}
